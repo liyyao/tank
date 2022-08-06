74b48d30c42008cb8dbe03c86c8d743b970b3481
liyyao <2813789717@qq.com>
2022-08-06 15:31:06 +0800

    添加键盘处理，根据上下左右判断坦克的方向

--- src/edu/hpu/liyy/tank/TankFrame.java
+++ src/edu/hpu/liyy/tank/TankFrame.java
@@ -29,22 +29,59 @@ public class TankFrame extends Frame {
 
     @Override
     public void paint(Graphics g) {
-        System.out.println("paint");
         g.fillRect(x, y, 50, 50);       //画出一个黑方块
-        x += 10;
+        //x += 10;
         //y += 10;
     }
 
     class MyKeyListener extends KeyAdapter {
+
+        boolean bL = false;
+        boolean bU = false;
+        boolean bR = false;
+        boolean bD = false;
         @Override
         public void keyPressed(KeyEvent e) {
+            int key = e.getKeyCode();
+            switch (key) {
+                case KeyEvent.VK_LEFT:
+                    bL = true;
+                    break;
+                case KeyEvent.VK_UP:
+                    bU = true;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    bR = true;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bD = true;
+                    break;
+                default:
+                    break;
+            }
             /*x += 20;
             repaint();*/
         }
 
         @Override
         public void keyReleased(KeyEvent e) {
-            System.out.println("key released");
+            int key = e.getKeyCode();
+            switch (key) {
+                case KeyEvent.VK_LEFT:
+                    bL = false;
+                    break;
+                case KeyEvent.VK_UP:
+                    bU = false;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    bR = false;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bD = false;
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
