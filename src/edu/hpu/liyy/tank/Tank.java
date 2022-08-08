56854aa8dc0b5172f836bc4011d7d5db06966cb7
liyyao <2813789717@qq.com>
2022-08-08 11:05:46 +0800

    坦克图片区分

--- src/edu/hpu/liyy/tank/Tank.java
+++ src/edu/hpu/liyy/tank/Tank.java
@@ -7,9 +7,9 @@ public class Tank {
 
     private int x, y;
     private Dir dir = Dir.DOWN;
-    private static final int SPEED = 1;
-    public static final int WIDTH = ResourceMgr.tankD.getWidth();
-    public static final int HEIGHT = ResourceMgr.tankD.getHeight();
+    private static final int SPEED = 5;
+    public static final int WIDTH = ResourceMgr.goodTankU.getWidth();
+    public static final int HEIGHT = ResourceMgr.goodTankU.getHeight();
     private boolean moving = true;
     private TankFrame tf = null;
     private boolean living = true;
@@ -62,16 +62,16 @@ public class Tank {
         }
         switch (dir) {
             case LEFT:
-                g.drawImage(ResourceMgr.tankL, x, y, null);
+                g.drawImage(this.group == Group.GOOD ? ResourceMgr.goodTankL : ResourceMgr.badTankL, x, y, null);
                 break;
             case UP:
-                g.drawImage(ResourceMgr.tankU, x, y, null);
+                g.drawImage(this.group == Group.GOOD ? ResourceMgr.goodTankU : ResourceMgr.badTankU, x, y, null);
                 break;
             case RIGHT:
-                g.drawImage(ResourceMgr.tankR, x, y, null);
+                g.drawImage(this.group == Group.GOOD ? ResourceMgr.goodTankR : ResourceMgr.badTankR, x, y, null);
                 break;
             case DOWN:
-                g.drawImage(ResourceMgr.tankD, x, y, null);
+                g.drawImage(this.group == Group.GOOD ? ResourceMgr.goodTankD : ResourceMgr.badTankD, x, y, null);
                 break;
         }
         move();
